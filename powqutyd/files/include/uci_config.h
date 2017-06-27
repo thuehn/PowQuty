ba867c7c210838712112c1a980f55b676f1ddc13
Stefan Venz <stefan.venz@protonmail.com>
2017-06-27 16:56:01 +0200

    Add slack options to config
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- powqutyd/files/include/uci_config.h
+++ powqutyd/files/include/uci_config.h
@@ -11,18 +11,26 @@
 #define MAX_LENGTH	32
 
 struct powquty_conf {
-
+	/* general */
 	char device_tty[MAX_LENGTH];
-	char mqtt_host[MAX_LENGTH];
-	char mqtt_topic[MAX_LENGTH];
-	char mqtt_uname[MAX_LENGTH];
-	char mqtt_pw[MAX_LENGTH];
 	char dev_uuid[MAX_LENGTH];
 	char dev_lat[MAX_LENGTH];
 	char dev_lon[MAX_LENGTH];
 	char powquty_path[MAX_LENGTH];
 	int powqutyd_print;
 	long max_log_size_kb;
+
+	/* mqtt */
+	char mqtt_host[MAX_LENGTH];
+	char mqtt_topic[MAX_LENGTH];
+	char mqtt_uname[MAX_LENGTH];
+	char mqtt_pw[MAX_LENGTH];
+
+	/* slack */
+	char slack_webhook[MAX_LENGTH];
+	char slack_channel[MAX_LENGTH];
+	char slack_user[MAX_LENGTH];
+	int slack_notification;
 
 /*	option device_tty '/dev/ttyACM0'
 		option mqtt_host 'localhost'
@@ -31,6 +39,10 @@ struct powquty_conf {
 		option dev_lat '51.156033'
 		option dev_lon '10.715828'
 		option powqutyd_print '1'
+		option slack_webhook ''
+		option slack_channel '#general'
+		option slack_user 'PowQutyEvent'
+		option slack_notification '0'
 		*/
 };
 
