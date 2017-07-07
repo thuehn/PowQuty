04f4ed51801a20a82eb41816db26f07950d5784e
Stefan Venz <stefan.venz@protonmail.com>
2017-07-07 16:57:54 +0200

    Add localtime
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- powqutyd/files/src/event_handling.c
+++ powqutyd/files/src/event_handling.c
@@ -3,16 +3,22 @@
  *  Created on Jun 27, 2017
  *  	Author: ikstream
  */
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "event_handling.h"
 #include "libwebslack.h"
 
-#define MAX_MSG_LENGTH 1024
-#define MAX_EVENT_LENGTH 64
+#define MAX_MSG_LENGTH		1024
+#define MAX_EVENT_LENGTH	64
+#define MAX_TIME_LENGTH		64
 
 void send_event(PQEvent pqe, struct powquty_conf *conf) {
+	time_t timer;
+	struct tm *tmi;
 	char *msg = malloc(sizeof(char) * MAX_MSG_LENGTH);
 	if (msg == NULL) {
 		printf("Could not allocate memory for msg in %s\n", __func__);
@@ -25,6 +31,13 @@ void send_event(PQEvent pqe, struct powquty_conf *conf) {
 		return;
 	}
 
+	char *local_time = malloc(sizeof(char) * MAX_TIME_LENGTH);
+	if (local_time == NULL) {
+		printf("Could not allocate memory for time in %s\n", __func__);
+		return;
+	}
+
+	/* set event type */
 	switch (pqe.type) {
 		case (int)PQ_EVENT_TYPE_DIP:
 			snprintf(event, MAX_EVENT_LENGTH, "Voltage dip >= 10%%");
@@ -43,12 +56,29 @@ void send_event(PQEvent pqe, struct powquty_conf *conf) {
 			break;
 	}
 
+	/* set local time */
+	time(&timer);
+	if (timer == -1) {
+		printf("Could not get time since epoch in %s\n", __func__);
+		if (errno)
+			printf("error is %s\n", strerror(errno));
+		return;
+	}
+
+	tmi = localtime(&timer);
+	if (tmi == NULL) {
+		printf("Error occurred in %s: %s\n", __func__, strerror(errno));
+		return;
+	}
+
+	strftime(local_time, MAX_TIME_LENGTH, "%Y-%m-%d %H:%M:%S", tmi);
+
+	/* prepare msg to send */
+	snprintf(msg, MAX_MSG_LENGTH, "%s started: %s", event, local_time);
+
 	/* free allocated stuff */
 	free(msg);
 	free(event);
-
-	/* prepare msg to send */
-	snprintf(msg, MAX_MSG_LENGTH, "%s", event);
 
 #ifdef SLACK
 	if (conf->slack_notification) {
@@ -78,6 +108,7 @@ void send_event(PQEvent pqe, struct powquty_conf *conf) {
 		}
 	}
 #endif /* Slack */
+	free(local_time);
 }
 
 void handle_event(PQResult pqResult, struct powquty_conf *conf) {
