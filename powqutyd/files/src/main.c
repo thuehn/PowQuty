a1f98c3a3be3ac2847553640c9d0074b584d22e7
Stefan Venz <stefan.venz@protonmail.com>
2018-05-23 16:05:20 +0200

    Clean up main
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- powqutyd/files/src/main.c
+++ powqutyd/files/src/main.c
@@ -24,11 +24,6 @@
 #endif
 
 static volatile int stop_main = 0;
-/*
-void lebenszeichen(){
-	printf("\t\t\t\t\tAlive\n");
-}
-*/
 
 void handle_signal()
 {
@@ -90,34 +85,21 @@ void handle_args (int argc, char **argv) {
 int main (int argc, char *argv[]) {
 	signal(SIGINT, handle_signal);
 	signal(SIGTERM, handle_signal);
-	// char* config_file = "/etc/powqutyd/powqutyd.cfg";
-
-	/*if(load_config(config_file)){
-		printf("Error: could not load some config from %s\n", config_file);
-		// return -1;
-	}*/
 
 	struct powquty_conf conf;
 	int conf_res = uci_config_powquty(&conf);
-	printf("UCI CONFIG returned %d\n",conf_res);
-
-	// PQ_ERROR err = PQ_NO_ERROR;
 
 	printf("Starting powqutyd ...\n");
 #ifdef MQTT
 	if(!mqtt_init(&conf)) {
-		printf("MQTT Thread started \n");
+		printf("DEBUG:\t\tMQTT Thread started \n");
 	} else {
-		printf("couldn't start MQTT-Thread\n");
-		// return -1;
+		printf("WARN:\t\tCouldn't start MQTT-Thread\n");
 	}
 #endif
 	handle_args(argc, argv);
 
-	printf("arguments handled\n");
 	if (get_input_file_state()) {
-		printf("use input file\n");
-//		sleep(10);
 		if (!file_read_init(&conf)) {
 #ifdef MQTT
 			publish_device_online();
@@ -129,7 +111,6 @@ int main (int argc, char *argv[]) {
 		}
 		return 0;
 	}
-	printf("not input_file\n");
 	if(!calculation_init(&conf)) {
 		printf("Calculation Thread started\n");
 #ifdef MQTT
@@ -146,9 +127,6 @@ int main (int argc, char *argv[]) {
 
 	while (!stop_main){
 		sleep(2);
-		// lebenszeichen();
-
-
 	}
 
 
